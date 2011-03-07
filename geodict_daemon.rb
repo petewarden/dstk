f317992859735b3c781dfce63916fe9fa61b1920
Pete Warden <pete@petewarden.com>
2011-03-07 00:02:58 -0800

    Updated daemon script

--- geodict_daemon.rb
+++ geodict_daemon.rb
@@ -1,26 +1,11 @@
-#***********************************************************************************
-#
-# All code (C) Pete Warden, 2011
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#***********************************************************************************
+#!/usr/bin/env ruby
 
 require 'rubygems'
 require 'daemons'
 
-Daemons.run('/home/ubuntu/sources/geodict_server.rb', {
+Daemons.run('/home/ubuntu/sources/geodictapi/geodict_server.rb', {
   :dir => '/opt/pids/sinatra/',
+  :dir_mode => :normal,
   :log_output => true,
 })
+
