ab0a4a33a9cd50543d7ad4b5fb235bc46ddcc0a9
Pete Warden <pete@petewarden.com>
2011-03-14 18:17:03 -0700

    Fixes for location2politics

--- geodict_server.rb
+++ geodict_server.rb
@@ -494,9 +494,9 @@ def location2politics(locations, callback=nil)
     lon = location[:longitude]
     
     point_string = 'setsrid(makepoint('
-      +PGconn.escape(lon)
+      +PGconn.escape(lon.to_s)
       +', '
-      +PGconn.escape(lat)
+      +PGconn.escape(lat.to_s)
       +'), 4326)'
 
     country_select = 'SELECT name,country_code FROM "world_countries_polygon" WHERE within('
