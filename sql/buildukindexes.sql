a55f14653f8833513eb09b4a38497a052c45fe0e
Pete Warden <pete@petewarden.com>
2011-04-10 15:32:54 -0700

    Working on optimizing UK lookups and making them insensitive to case

--- sql/buildukindexes.sql
+++ sql/buildukindexes.sql
@@ -1,5 +1,5 @@
 -- Builds indexes for the common queries on these tables
 
-CREATE INDEX ON uk_osm_point ((lower(name)));
-CREATE INDEX ON uk_osm_line ((lower(name)));
+CREATE INDEX uk_osm_point_name_idx ON uk_osm_point ((lower(name)));
+CREATE INDEX uk_osm_line_name_idx ON uk_osm_line ((lower(name)));
 
