465e697fe48c3cbe638ce59beb12c3138feb28c3
Pete Warden <pete@jetpac.com>
2013-05-24 15:08:08 -0700

    Load name information into database

--- sql/loadnames.sql
+++ sql/loadnames.sql
@@ -15,3 +15,16 @@ CREATE TABLE ethnicity_of_surnames(
 COPY ethnicity_of_surnames(name, rank, count, prop100k, cum_prop100k, pctwhite, pctblack, pctapi, pctaian, pct2prace, pcthispanic)
   FROM '/home/ubuntu/sources/dstkdata/ethnicityofsurnames.csv'
   WITH DELIMITER AS ',' CSV HEADER;
+  
+CREATE TABLE first_names(
+  name CHAR(16) PRIMARY KEY,
+  count INT,
+  male_percentage FLOAT,
+  most_popular_year INT,
+  earliest_common_year INT,
+  latest_common_year INT
+);
+
+COPY first_names(name, count, male_percentage, most_popular_year, earliest_common_year, latest_common_year)
+  FROM './babynames.csv'
+  WITH DELIMITER AS ',' CSV HEADER;
