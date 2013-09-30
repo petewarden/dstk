17c5a44859457af68ec4be30d9af883b5e4e2c44
Pete Warden <pete@jetpac.com>
2013-09-30 15:19:05 -0700

    Debugging upstart problems

--- twofishesd.sh
+++ twofishesd.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# The default locale for Upstart is empty, which causes Unicode argument
+# The default locale for Upstart scripts is empty, which causes unicode argument
 # parsing to silently fail in the TwoFishes Java server code!
 export LANG=en_US.UTF-8
-locale
-java -jar /home/ubuntu/sources/twofishes/bin/twofishes.jar --hfile_basepath /home/ubuntu/sources/twofishes/data/latest/ &
+java -jar /home/ubuntu/sources/twofishes/bin/twofishes.jar --hfile_basepath /home/ubuntu/sources/twofishes/data/latest/
