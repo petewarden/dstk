199e79e6cf3d538ed30d322869a5edf00c9e863d
zslayton <zack.slayton@gmail.com>
2014-02-22 12:17:55 -0500

    Increased the maximum heap space for twofishes' JVM to avoid an OOM exception on startup. Added EC2 instance size recommendation to the docs.

--- twofishesd.sh
+++ twofishesd.sh
@@ -2,4 +2,4 @@
 # The default locale for Upstart scripts is empty, which causes unicode argument
 # parsing to silently fail in the TwoFishes Java server code!
 export LANG=en_US.UTF-8
-java -jar /home/ubuntu/sources/twofishes/bin/twofishes.jar --hfile_basepath /home/ubuntu/sources/twofishes/data/latest/
+java -Xmx1500M -jar /home/ubuntu/sources/twofishes/bin/twofishes.jar --hfile_basepath /home/ubuntu/sources/twofishes/data/latest/
